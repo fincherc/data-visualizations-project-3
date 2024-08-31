590c2389beebbd2ad7964d55885819b3e99ebff6
fincherc <christian.fincher@gmail.com>
2024-08-31 11:21:52 -0500

    Topo layer chugs my system; removing

--- fincherc-work/leaflet_work/script_2.js
+++ fincherc-work/leaflet_work/script_2.js
@@ -13,11 +13,6 @@ let street = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-// let topo = L.tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
-//   attribution:
-//     'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-// }).addTo(myMap);
-
 // Initialize arrays to hold the markers for sunny and overcast accidents
 // Create a new marker cluster group.
 let sunnyMarkers = L.markerClusterGroup({
